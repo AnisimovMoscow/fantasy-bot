7554eb605193ac956b0de719866eda0be6593bdb
Anton Anisimov <anton.v.anisimov@gmail.com>
2017-03-26 13:34:36 +0300

    Просмотр составов

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -24,25 +24,24 @@ class SiteController extends Controller
     public function actionHook() {
         $update = Yii::$app->request->post();
         Yii::info(print_r($update, true), 'hook');
-        
-        if (isset($update['message'])) {
-            $message = $update['message'];
-        } elseif (isset($update['edited_message'])) {
-            $message = $update['edited_message'];
-        }
-        
-        if ($message['chat']['type'] == 'private' && isset($message['text'])) {
-            $params = explode(' ', $message['text']);
+
+        if ($update['message']['chat']['type'] == 'private' && isset($update['message']['text'])) {
+            $params = explode(' ', $update['message']['text']);
             $command = array_shift($params);
             $method = 'command'.ucfirst(ltrim($command, '/'));
             if (method_exists($this, $method) && is_callable([$this, $method])) {
-                $this->$method($params, $message['chat']);
+                $this->$method($params, $update['message']['chat']);
             } else {
-                $this->unknownCommand($message['chat']);
+                preg_match('/https:\/\/www\.sports\.ru\/fantasy\/football\/team[\/points]*\/(\d+)\.html/', $command, $id);
+                if (empty($id[1]) || !is_numeric($id[1])) {
+                    $this->unknownCommand($update['message']['chat']);
+                } else {
+                    $this->commandTeam($id[1], $update['message']['chat']);
+                }
             }
         }
     }
-
+    
     /**
      * Начало работы, регистрация пользователя
      */
@@ -77,7 +76,7 @@ class SiteController extends Controller
      */
     public function commandProfile($params, $chat) {
         if (count($params) == 1) {
-            if (preg_match('/^https:\/\/www\.sports\.ru\/profile\/\d+\/$/', $params[0])) {
+            if (preg_match('/^http:\/\/www\.sports\.ru\/profile\/\d+\/$/', $params[0])) {
                 $user = User::findOne(['chat_id' => $chat['id']]);
                 if ($user === null) {
                     $message = 'Кажется мы ещё не здоровались. Отправь мне /start';
@@ -93,7 +92,7 @@ class SiteController extends Controller
                     }
                 }
             } else {
-                $message = 'Ты мне неправильно отправил ссылку. Просто зайди на сайт sports.ru и скопируй ссылку на свою страницу. Там должны быть ссылка вида https://www.sports.ru/profile/12345/';
+                $message = 'Ты мне неправильно отправил ссылку. Просто зайди на сайт sports.ru и скопируй ссылку на свою страницу. Там должны быть ссылка вида http://www.sports.ru/profile/12345/';
             }
         } else {
             $message = 'Нужно просто написать /profile и через пробел ссылку на свой профиль';
@@ -220,7 +219,53 @@ class SiteController extends Controller
         
         $this->send($chat['id'], $message);
     }
-
+    
+    /**
+     * Просмотр состава команды
+     */
+    public function commandTeam($id, $chat) {
+        $json = file_get_contents('https://www.sports.ru/fantasy/football/team/json/'.$id.'.json');
+        if ($json === false) {
+            $message = 'Ошибка при загрузке данных.';
+        } else {
+            $team = json_decode($json);
+            if (is_null($team)) {
+                $message = 'Ошибка при обработке данных.';
+            } else {
+                $ampluaName = [
+                    1 => 'вр',
+                    2 => 'защ',
+                    3 => 'пз',
+                    4 => 'нап',
+                ];
+                $message = 'Основа:'."\n";
+                foreach ($team->players as $player) {
+                    if ($player->row > 0) {
+                        $message .= $player->name.' (';
+                        $message .= $player->club.') ';
+                        if ($player->isCaptain == '1') {
+                            $message .= '[c] ';
+                        }
+                        $message .= '- '.$ampluaName[$player->amplua].', ';
+                        $message .= $player->price."\n";
+                    }
+                }
+                $message .= "\n";
+                $message .= 'Запасные:'."\n";
+                foreach ($team->players as $player) {
+                    if ($player->row == '0') {
+                        $message .= $player->name.' (';
+                        $message .= $player->club.') ';
+                        $message .= '- '.$ampluaName[$player->amplua].', ';
+                        $message .= $player->price."\n";
+                    }
+                }
+            }
+        }
+        
+        $this->send($chat['id'], $message);
+    }
+    
     /**
      * Неизвестная команда, ошибка
      */
