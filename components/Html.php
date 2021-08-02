8356d22464137dbd1c94cbd3bc1af126f35a3674
Антон Анисимов <anton.v.anisimov@gmail.com>
2021-08-02 21:52:46 +0300

    Ретраи в curl

--- components/Html.php
+++ components/Html.php
@@ -22,9 +22,14 @@ class Html extends BaseObject
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36');
-        $html = curl_exec($ch);
-        if ($html === false) {
-            Yii::info('Curl error. URL: ' . $url . ' Error: ' . curl_error($ch), 'curl');
+
+        $retry = 0;
+        while ($retry < 3) {
+            $html = curl_exec($ch);
+            if ($html === false) {
+                Yii::info('Curl error. URL: ' . $url . ' Error: ' . curl_error($ch) . ' Retry: ' . $retry, 'curl');
+            }
+            $retry++;
         }
         curl_close($ch);
         
