bd519d1d7ddd542e57e2eb307167b0135122008b
Антон Анисимов <anton.v.anisimov@gmail.com>
2022-07-23 22:38:35 +0300

    Ошибка при загрузке настроек

--- components/Telegram.php
+++ components/Telegram.php
@@ -18,7 +18,7 @@ class Telegram extends Component
         }
         sort($data_check_arr);
         $data_check_string = implode("\n", $data_check_arr);
-        $secret_key = hash_hmac('sha256', Yii::$app->params['token']['ru'], 'WebAppData', true);
+        $secret_key = hash_hmac('sha256', Yii::$app->params['token'], 'WebAppData', true);
 
         $hash = bin2hex(hash_hmac('sha256', $data_check_string, $secret_key, true));
         return (strcmp($hash, $check_hash) === 0);
